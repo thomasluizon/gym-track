276bec7174c50a42d9bae1850879c9e05c274287
thomasluizon <thomaslrgregorio@gmail.com>
2023-08-26 17:25:52 -0300

    Add path alias and colors object

---
+++ src/utils/colors.ts
@@ -0,0 +1,15 @@
+type Colors = {
+	color1: string
+	color2: string
+	color3: string
+	color4: string
+	color5: string
+}
+
+export const colors: Colors = {
+	color1: '#040B21',
+	color2: '#698EFF',
+	color3: '#1C55FF',
+	color4: '#F5F5F5',
+	color5: '#061133',
+}
